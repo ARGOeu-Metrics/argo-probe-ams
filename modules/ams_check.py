9425dda24fd1377e767c5a97b10512f7abc20b4a
Dean Hudek <hudek.dean@gmail.com>
2022-10-21 11:18:42 +0200

    code formatted and test fixed

--- modules/ams_check.py
+++ modules/ams_check.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-import random, string, hashlib
+import random
+import string
+import hashlib
 
 from argparse import ArgumentParser
 from argo_ams_library import ArgoMessagingService, AmsMessage, AmsException, AmsMessageException
@@ -12,107 +14,105 @@ msg_orig = set()
 
 
 def utils(arguments):
-    
+
     nagios = NagiosResponse("All messages received correctly.")
-    ams = ArgoMessagingService(endpoint=arguments.host, token=arguments.token, project=arguments.project)
+    ams = ArgoMessagingService(
+        endpoint=arguments.host, token=arguments.token, project=arguments.project)
 
     try:
-        
-        if ams.has_topic(arguments.topic, timeout=arguments.timeout):        
+
+        if ams.has_topic(arguments.topic, timeout=arguments.timeout):
             ams.delete_topic(arguments.topic, timeout=arguments.timeout)
-        
+
         if ams.has_sub(arguments.subscription, timeout=arguments.timeout):
             ams.delete_sub(arguments.subscription, timeout=arguments.timeout)
-        
-        ams.create_topic(arguments.topic, timeout=arguments.timeout) 
-        ams.create_sub(arguments.subscription, arguments.topic, timeout=arguments.timeout) 
-        
-    
-    
+
+        ams.create_topic(arguments.topic, timeout=arguments.timeout)
+        ams.create_sub(arguments.subscription, arguments.topic,
+                       timeout=arguments.timeout)
+
     except AmsException as e:
         nagios.writeCriticalMessage(e.msg)
         nagios.setCode(nagios.CRITICAL)
-        print(nagios.getMsg()) 
+        print(nagios.getMsg())
         raise SystemExit(nagios.getCode())
-    
 
-    
-    ams_msg = AmsMessage() 
+    ams_msg = AmsMessage()
     msg_array = []
-    
-    
-    
-    try:  
-        for i in range(1, MSG_NUM):        
-            msg_txt = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(MSG_SIZE))
-            attr_name = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(4))
-            attr_value = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(8))
-            msg_array.append(ams_msg(data=msg_txt, attributes={attr_name: attr_value}))
+
+    try:
+        for i in range(1, MSG_NUM):
+            msg_txt = ''.join(random.choice(
+                string.ascii_letters + string.digits) for i in range(MSG_SIZE))
+            attr_name = ''.join(random.choice(
+                string.ascii_letters + string.digits) for i in range(4))
+            attr_value = ''.join(random.choice(
+                string.ascii_letters + string.digits) for i in range(8))
+            msg_array.append(
+                ams_msg(data=msg_txt, attributes={attr_name: attr_value}))
             hash_obj = hashlib.md5((msg_txt + attr_name + attr_value).encode())
             msg_orig.add(hash_obj.hexdigest())
-
-            
 
     except (AmsMessageException, TypeError, AttributeError):
         nagios.setCode(nagios.CRITICAL)
         print(nagios.getMsg())
         raise SystemExit(2)
 
-    
     try:
-        msgs = ams.publish(arguments.topic, msg_array, timeout=arguments.timeout)
+        msgs = ams.publish(arguments.topic, msg_array,
+                           timeout=arguments.timeout)
 
         ackids = []
         rcv_msg = set()
 
-        for id, msg in ams.pull_sub(arguments.subscription, MSG_NUM - 1, True, timeout=arguments.timeout):     
+        for id, msg in ams.pull_sub(arguments.subscription, MSG_NUM - 1, True, timeout=arguments.timeout):
             attr = msg.get_attr()
 
-            hash_obj = hashlib.md5((msg.get_data().decode('utf-8') + list(attr.keys())[0] + list(attr.values())[0]).encode())
+            hash_obj = hashlib.md5((msg.get_data().decode(
+                'utf-8') + list(attr.keys())[0] + list(attr.values())[0]).encode())
             rcv_msg.add(hash_obj.hexdigest())
             ackids.append(id)
-        
+
         if ackids:
-            ams.ack_sub(arguments.subscription, ackids, timeout=arguments.timeout)
-      
-        ams.delete_topic(arguments.topic, timeout=arguments.timeout) 
+            ams.ack_sub(arguments.subscription, ackids,
+                        timeout=arguments.timeout)
+
+        ams.delete_topic(arguments.topic, timeout=arguments.timeout)
         ams.delete_sub(arguments.subscription, timeout=arguments.timeout)
 
-    
-    
-    except AmsException as e:    
+    except AmsException as e:
         nagios.writeCriticalMessage(e.msg)
         nagios.setCode(nagios.CRITICAL)
         print(nagios.getMsg())
         raise SystemExit(nagios.getCode())
-    
-    
+
     if msg_orig != rcv_msg:
         nagios.writeCriticalMessage("Messages received incorrectly.")
         nagios.setCode(nagios.CRITICAL)
 
-
     print(nagios.getMsg())
     raise SystemExit(nagios.getCode())
-    
 
 
 def main():
     TIMEOUT = 180
 
     parser = ArgumentParser(description="Nagios sensor for AMS")
-    parser.add_argument('-H', dest='host', type=str, default='messaging-devel.argo.grnet.gr', help='FQDN of AMS Service')
+    parser.add_argument('-H', dest='host', type=str,
+                        default='messaging-devel.argo.grnet.gr', help='FQDN of AMS Service')
     parser.add_argument('--token', type=str, required=True, help='Given token')
-    parser.add_argument('--project', type=str, required=True, help='Project registered in AMS Service')
-    parser.add_argument('--topic', type=str, default='nagios_sensor_topic', help='Given topic')
-    parser.add_argument('--subscription', type=str, default='nagios_sensor_sub', help='Subscription name')
-    parser.add_argument('-t', dest='timeout', type=int, default=TIMEOUT, help='Timeout')
+    parser.add_argument('--project', type=str, required=True,
+                        help='Project registered in AMS Service')
+    parser.add_argument('--topic', type=str,
+                        default='nagios_sensor_topic', help='Given topic')
+    parser.add_argument('--subscription', type=str,
+                        default='nagios_sensor_sub', help='Subscription name')
+    parser.add_argument('-t', dest='timeout', type=int,
+                        default=TIMEOUT, help='Timeout')
     cmd_options = parser.parse_args()
 
     utils(arguments=cmd_options)
 
 
-
 if __name__ == "__main__":
     main()
-
--- tests/test_probe_ams.py
+++ tests/test_probe_ams.py
@@ -1,9 +1,9 @@
 from types import SimpleNamespace
 import unittest
 from unittest.mock import patch
-import hashlib
 
-from argo_ams_library import AmsConnectionException, AmsMessage
+
+from argo_ams_library import AmsConnectionException, AmsException, AmsMessage
 from argo_probe_ams.NagiosResponse import NagiosResponse
 from argo_probe_ams.ams_check import utils
 
@@ -11,21 +11,22 @@ from argo_probe_ams.ams_check import utils
 def mock_pull_sub_func(*args, **kwargs):
     return [('projects/mock_PROJECT/subscriptions/mock_sensor_sub:0', AmsMessage)]
 
+
 def mock_pass_func(*args, **kwargs):
     pass
 
-def mock_func_ams_message(*args, **kwargs):
-    return AmsMessage(data="nest", attributes="svast")
 
+def mock_func_ams_message(*args, **kwargs):
+    return AmsMessage(data="mock_data", attributes="mock_attributes")
 
 
 class ArgoProbeAmsTests(unittest.TestCase):
 
     def setUp(self) -> None:
-        arguments = {"host": "mock_host", "token": "1234", "project": "mock_PROJECT", "topic": "mock_topic", "timeout": 3, "subscription": "mock_sensor_sub"}
+        arguments = {"host": "mock_host", "token": "1234", "project": "mock_PROJECT",
+                     "topic": "mock_topic", "timeout": 3, "subscription": "mock_sensor_sub"}
         self.arguments = SimpleNamespace(**arguments)
 
- 
     @patch("argo_probe_ams.ams_check.hashlib")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.create_sub")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.create_topic")
@@ -38,70 +39,82 @@ class ArgoProbeAmsTests(unittest.TestCase):
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.publish")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.has_sub")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.has_topic")
-    def test_pass_utils_func(self, mock_has_topic, mock_has_sub, mock_publish, mock_pull_sub, mock_get_attr, mock_get_data, mock_ack_sub , mock_delete_topic, mock_delete_sub, mock_create_topic, mock_create_sub, mock_hashlib): 
-      
+    def test_all_passed(self, mock_has_topic, mock_has_sub, mock_publish,
+                        mock_pull_sub, mock_get_attr, mock_get_data,
+                        mock_ack_sub, mock_delete_topic, mock_delete_sub,
+                        mock_create_topic, mock_create_sub, mock_hashlib):
         mock_has_topic.return_value = False
         mock_has_sub.return_value = False
         mock_publish.return_value = {'messageIds': ['0']}
         mock_pull_sub.side_effect = mock_pull_sub_func
         mock_get_attr.return_value = {'mock_key': 'mock_value'}
         mock_get_data.return_value = b'mock_bytes'
-        mock_ack_sub.return_value = False
-        mock_hashlib.return_value = "mock_rand"
+        mock_ack_sub.return_value = True
         mock_delete_topic.side_effect = mock_pass_func
         mock_delete_sub.side_effect = mock_pass_func
-        
+        mock_hashlib.return_value = "mock_rand"
 
         with self.assertRaises(SystemExit) as e:
             utils(self.arguments)
 
-        mock_create_topic.assert_called_once_with(self.arguments.topic, timeout=self.arguments.timeout)
-        mock_create_sub.assert_called_once_with(self.arguments.subscription, self.arguments.topic, timeout=self.arguments.timeout)
-        
-        self.assertEqual(e.exception.__str__(), "0")
+        mock_create_topic.assert_called_once_with(
+            self.arguments.topic, timeout=self.arguments.timeout)
+        mock_create_sub.assert_called_once_with(
+            self.arguments.subscription, self.arguments.topic, timeout=self.arguments.timeout)
 
-
-
+        self.assertEqual(e.exception.code, 0)
 
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.has_topic")
-    def test_catch_first_exception(self, mock_has_topic):
-
-        mock_has_topic.side_effect = ([AmsConnectionException("failed connection", "http_mock_get")], )
+    def test_connectionerror_on_hastopic(self, mock_has_topic):
+        mock_has_topic.side_effect = AmsConnectionException(
+            "failed connection", "http_mock_get")
 
         with self.assertRaises(SystemExit) as e:
             utils(self.arguments)
 
         mock_has_topic.assert_called_once()
-        self.assertEqual(e.exception.__str__(), "2")
+        self.assertEqual(e.exception.code, 2)
 
-
-
-
+    @patch("argo_probe_ams.ams_check.ArgoMessagingService.delete_sub")
+    @patch("argo_probe_ams.ams_check.ArgoMessagingService.delete_topic")
+    @patch("argo_probe_ams.ams_check.ArgoMessagingService.create_sub")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.create_topic")
+    @patch("argo_probe_ams.ams_check.ArgoMessagingService.ack_sub")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.publish")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.has_sub")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.has_topic")
-    def test_catch_third_exception(self, mock_has_topic, mock_has_sub, mock_publish, mock_create_topic):
-      
+    def test_connectionerror_on_amspublish(self, mock_has_topic, mock_has_sub,
+                                           mock_publish, mock_ack_sub,
+                                           mock_create_topic,
+                                           mock_create_sub,
+                                           mock_delete_topic,
+                                           mock_delete_sub):
         mock_has_topic.return_value = False
         mock_has_sub.return_value = False
-        mock_publish.side_effect = ([AmsConnectionException("failed connection", "http_mock_get")], )
+        mock_publish.return_value = False
 
         with self.assertRaises(SystemExit) as e:
             utils(self.arguments)
 
-        mock_create_topic.assert_called_once_with(self.arguments.topic, timeout=self.arguments.timeout)
-        self.assertEqual(e.exception.__str__(), "2")
+        mock_create_topic.assert_called_once_with(
+            self.arguments.topic, timeout=self.arguments.timeout)
+        mock_create_sub.assert_called_once_with(
+            self.arguments.subscription, self.arguments.topic, timeout=self.arguments.timeout)
 
+        self.assertFalse(mock_ack_sub.called)
+        self.assertFalse(mock_delete_topic.called)
+        self.assertFalse(mock_delete_sub.called)
 
-
+        self.assertEqual(e.exception.code, 2)
 
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.create_sub")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.create_topic")
     @patch("argo_probe_ams.ams_check.AmsMessage")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.has_sub")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.has_topic")
-    def test_catch_second_exception_ams_msg(self, mock_has_topic, mock_has_sub, mock_ams_msg, mock_create_topic, mock_create_sub):
+    def test_connectionerror_on_amsmessage(self, mock_has_topic, mock_has_sub, mock_ams_msg,
+                                           mock_create_topic,
+                                           mock_create_sub):
 
         mock_has_topic.return_value = False
         mock_has_sub.return_value = False
@@ -110,13 +123,12 @@ class ArgoProbeAmsTests(unittest.TestCase):
         with self.assertRaises(SystemExit) as e:
             utils(self.arguments)
 
-        mock_create_topic.assert_called_once_with(self.arguments.topic, timeout=self.arguments.timeout)
-        mock_create_sub.assert_called_once_with(self.arguments.subscription, self.arguments.topic, timeout=self.arguments.timeout)       
+        mock_create_topic.assert_called_once_with(
+            self.arguments.topic, timeout=self.arguments.timeout)
+        mock_create_sub.assert_called_once_with(
+            self.arguments.subscription, self.arguments.topic, timeout=self.arguments.timeout)
 
-        self.assertEqual(e.exception.__str__(), "2")
-     
-    
-
+        self.assertEqual(e.exception.code, 2)
 
     @patch("argo_probe_ams.ams_check.msg_orig")
     @patch("argo_probe_ams.ams_check.random.choice")
@@ -127,7 +139,10 @@ class ArgoProbeAmsTests(unittest.TestCase):
     @patch("argo_probe_ams.ams_check.AmsMessage")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.has_sub")
     @patch("argo_probe_ams.ams_check.ArgoMessagingService.has_topic")
-    def test_catch_hexdigest(self, mock_has_topic, mock_has_sub, mock_ams_msg, mock_create_topic, mock_create_sub, mock_random_choice, mock_msg_orig):
+    def test_expected_hashlibmd5_and_connectionerror(self, mock_has_topic, mock_has_sub,
+                                                     mock_ams_msg, mock_create_topic,
+                                                     mock_create_sub,
+                                                     mock_random_choice, mock_msg_orig):
 
         mock_has_topic.return_value = False
         mock_has_sub.return_value = False
@@ -137,29 +152,14 @@ class ArgoProbeAmsTests(unittest.TestCase):
         with self.assertRaises(SystemExit) as e:
             utils(self.arguments)
 
-        mock_create_topic.assert_called_once_with(self.arguments.topic, timeout=self.arguments.timeout)
-        mock_create_sub.assert_called_once_with(self.arguments.subscription, self.arguments.topic, timeout=self.arguments.timeout)
+        mock_create_topic.assert_called_once_with(
+            self.arguments.topic, timeout=self.arguments.timeout)
+        mock_create_sub.assert_called_once_with(
+            self.arguments.subscription, self.arguments.topic, timeout=self.arguments.timeout)
 
-        expected = "e1afc18e33d67cc202bb6056c28013ee"      
-  
+        expected = "e1afc18e33d67cc202bb6056c28013ee"
         mock_msg_orig.add.assert_called_once_with(expected)
-        self.assertEqual(e.exception.__str__(), "2")
-
-
-
-
-        
-
-
-        
-
-
-
-
-
-
-
-
+        self.assertEqual(e.exception.code, 2)
 
 
 if __name__ == '__main__':
