9abc84f6db3149b0d0a8928f11bfb2fa31b2c3a5
Daniel Vrcic <daniel.vrcic@gmail.com>
2022-12-04 14:20:46 +0100

    tox configuration for tests bootstrap

--- tests/test_probe_ams.py
+++ tests/test_probe_ams.py
@@ -11,7 +11,6 @@ from argo_probe_ams.ams_check import utils
 def mock_pull_sub_func(*args, **kwargs):
     return [('projects/mock_PROJECT/subscriptions/mock_sensor_sub:0', AmsMessage)]
 
-
 def mock_pass_func(*args, **kwargs):
     pass
 
@@ -19,9 +18,7 @@ def mock_pass_func(*args, **kwargs):
 def mock_func_ams_message(*args, **kwargs):
     return AmsMessage(data="mock_data", attributes="mock_attributes")
 
-
 class ArgoProbeAmsTests(unittest.TestCase):
-
     def setUp(self) -> None:
         arguments = {"host": "mock_host", "token": "1234", "project": "mock_PROJECT",
                      "topic": "mock_topic", "timeout": 3, "subscription": "mock_sensor_sub"}
