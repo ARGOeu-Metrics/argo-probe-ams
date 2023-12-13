8c13ba6691759fd3615a0ddafab13dcb1a18a6e4
Dean Hudek <hudek.dean@gmail.com>
2023-12-13 10:15:06 +0100

    tmp file stores latest topic/sub names

--- modules/ams_check.py
+++ modules/ams_check.py
@@ -3,6 +3,8 @@
 import random
 import string
 import hashlib
+import tempfile
+import os
 
 from argparse import ArgumentParser
 from argo_ams_library import ArgoMessagingService, AmsMessage, AmsException, AmsMessageException
@@ -10,32 +12,31 @@ from argo_probe_ams.NagiosResponse import NagiosResponse
 
 MSG_NUM = 100
 MSG_SIZE = 500
-msg_orig = set()
 
 
-def utils(arguments):
-    nagios = NagiosResponse("All messages received correctly.")
-    ams = ArgoMessagingService(
-        endpoint=arguments.host, token=arguments.token, project=arguments.project)
+temp_file_path = os.path.join("/var/spool/argo/probes/argo-probe-ams", "ams_cleanup.txt")  
+                                                                              
+def write_to_temp_file(host, topic, subscription):                     
+    with open(temp_file_path, 'w') as f:
+        f.write(f"{host}\n{topic}\n{subscription}\n")
 
-    try:
-        if ams.has_topic(arguments.topic, timeout=arguments.timeout):
-            ams.delete_topic(arguments.topic, timeout=arguments.timeout)
+def cleanup_from_temp_file(ams, current_host):
+    if os.path.exists(temp_file_path):
+        with open(temp_file_path, 'r') as f:
+            lines = f.readlines()
+            hashed_host = lines[0].strip()
+            topic = lines[1].strip()
+            subscription = lines[2].strip()
+           
+            if ams.has_topic(topic):
+                ams.delete_topic(topic)
+            if ams.has_sub(subscription):
+                ams.delete_sub(subscription)
 
-        if ams.has_sub(arguments.subscription, timeout=arguments.timeout):
-            ams.delete_sub(arguments.subscription, timeout=arguments.timeout)
 
-        ams.create_topic(arguments.topic, timeout=arguments.timeout)
-        ams.create_sub(arguments.subscription, arguments.topic,
-                       timeout=arguments.timeout)
-
-    except AmsException as e:
-        nagios.writeCriticalMessage(e.msg)
-        nagios.setCode(nagios.CRITICAL)
-        print(nagios.getMsg())
-        raise SystemExit(nagios.getCode())
-
+def create_msg(nagios):
     ams_msg = AmsMessage()
+    msg_orig = set()
     msg_array = []
 
     try:
@@ -56,10 +57,12 @@ def utils(arguments):
         print(nagios.getMsg())
         raise SystemExit(2)
 
-    try:
-        msgs = ams.publish(arguments.topic, msg_array,
-                           timeout=arguments.timeout)
+    return msg_array, msg_orig
 
+
+def publish_msgs(ams, arguments, msg_array, nagios):
+    try:
+        ams.publish(arguments.topic, msg_array, timeout=arguments.timeout)
 
         ackids = []
         rcv_msg = set()
@@ -76,8 +79,7 @@ def utils(arguments):
             ams.ack_sub(arguments.subscription, ackids,
                         timeout=arguments.timeout)
 
-        ams.delete_topic(arguments.topic, timeout=arguments.timeout)
-        ams.delete_sub(arguments.subscription, timeout=arguments.timeout)
+        return rcv_msg
 
     except AmsException as e:
         nagios.writeCriticalMessage(e.msg)
@@ -85,16 +87,52 @@ def utils(arguments):
         print(nagios.getMsg())
         raise SystemExit(nagios.getCode())
 
-    if msg_orig != rcv_msg:
-        nagios.writeCriticalMessage("Messages received incorrectly.")
-        nagios.setCode(nagios.CRITICAL)
 
-    print(nagios.getMsg())
-    raise SystemExit(nagios.getCode())
+def utils(arguments):
+    nagios = NagiosResponse("All messages received correctly.")
+
+    try:
+        ams = ArgoMessagingService(
+            endpoint=arguments.host, token=arguments.token, project=arguments.project)
+
+        cleanup_from_temp_file(ams, arguments.host)
+
+        if ams.has_topic(arguments.topic, timeout=arguments.timeout):
+            ams.delete_topic(arguments.topic, timeout=arguments.timeout)
+
+        if ams.has_sub(arguments.subscription, timeout=arguments.timeout):
+            ams.delete_sub(arguments.subscription, timeout=arguments.timeout)
+
+        ams.create_topic(arguments.topic, timeout=arguments.timeout)
+        ams.create_sub(arguments.subscription, arguments.topic,
+                       timeout=arguments.timeout)
+        
+        write_to_temp_file(arguments.host, arguments.topic, arguments.subscription)
+               
+        msg_array, msg_orig = create_msg(nagios)
+        rcv_msg = publish_msgs(ams, arguments, msg_array, nagios)
+
+        ams.delete_topic(arguments.topic, timeout=arguments.timeout)
+        ams.delete_sub(arguments.subscription, timeout=arguments.timeout)
+
+        if msg_orig != rcv_msg:
+            nagios.writeCriticalMessage("Messages received incorrectly.")
+            nagios.setCode(nagios.CRITICAL)
+
+        print(nagios.getMsg())
+        raise SystemExit(nagios.getCode())
+
+    except AmsException as e:
+        nagios.setCode(nagios.CRITICAL)
+        nagios.writeCriticalMessage(e.msg)
+        print(nagios.getMsg())
+        raise SystemExit(nagios.getCode())
+    
+
 
 def main():
     TIMEOUT = 180
-    random_bits = random.getrandbits(128)
+    random_bits = ("%032x" % random.getrandbits(128))[:16]
 
     parser = ArgumentParser(description="Nagios sensor for AMS")
     parser.add_argument('-H', dest='host', type=str,
@@ -103,14 +141,15 @@ def main():
     parser.add_argument('--project', type=str, required=True,
                         help='Project registered in AMS Service')
     parser.add_argument('--topic', type=str,
-                        default=("%032x" % random.getrandbits(128))[:16], help='Given topic')
+                        default=f"test_topic_{random_bits}", help='Given topic')
     parser.add_argument('--subscription', type=str,
-                        default=("%032x" % random.getrandbits(128))[:16], help='Subscription name')
+                        default=f"test_subsc_{random_bits}", help='Subscription name')
     parser.add_argument('-t', dest='timeout', type=int,
                         default=TIMEOUT, help='Timeout')
     cmd_options = parser.parse_args()
 
     utils(arguments=cmd_options)
+
 
 if __name__ == "__main__":
     main()
